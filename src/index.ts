d1e12b4639782b05684787d88a0ca05d8cd87478
Valentin <valentin.furmanek@gmail.com>
2024-02-12 20:27:56 +0100

    feat: initialize and watch the service

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
 import "reflect-metadata";
+import { TYPES } from "./inversify/types";
+import { logger } from "./inversify/providers/logger";
+import { createService } from "./service";
+
+async function main() {
+  const service = createService({
+    providers: [{ provide: TYPES.Logger, useValue: logger }],
+  });
+  await service.watch();
+}
+
+main();
