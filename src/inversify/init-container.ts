8b3890d2179228172fd38d755c56c536727dff2c
Valentin <valentin.furmanek@gmail.com>
2024-02-12 20:32:35 +0100

    feat: allow passing a provider to the container initialization function

--- src/inversify/init-container.ts
+++ src/inversify/init-container.ts
@@ -1,9 +1,24 @@
-import { Container } from "inversify";
+import { Container, interfaces } from "inversify";
 
-export interface ContainerInitializationOptions {}
+export type ContainerInitializationProvider = Parameters<
+  interfaces.BindingToSyntax<unknown>["to"]
+>[0];
 
-export function initContainer({}: ContainerInitializationOptions): Container {
+export interface ContainerInitializationOptions {
+  providers?: Array<
+    | ContainerInitializationProvider
+  >;
+}
+
+export function initContainer({
+  providers,
+}: ContainerInitializationOptions): Container {
   const container = new Container();
+  providers
+    ?.filter(function (provider): provider is ContainerInitializationProvider {
+      return typeof provider === "function";
+    })
+    .forEach((provider) => container.bind(provider).to(provider));
 
   return container;
 }
