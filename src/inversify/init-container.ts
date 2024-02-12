75b7d2a5617c6288be9e9ccbeb6c16785c3e232e
Valentin <valentin.furmanek@gmail.com>
2024-02-12 20:33:22 +0100

    feat: allow passing a static provider to the container initialization function

--- src/inversify/init-container.ts
+++ src/inversify/init-container.ts
@@ -3,10 +3,17 @@ import { Container, interfaces } from "inversify";
 export type ContainerInitializationProvider = Parameters<
   interfaces.BindingToSyntax<unknown>["to"]
 >[0];
+export type ContainerInitializationStaticProvider = {
+  provide: interfaces.ServiceIdentifier<unknown>;
+  useValue: Parameters<
+    interfaces.BindingToSyntax<unknown>["toConstantValue"]
+  >[0];
+};
 
 export interface ContainerInitializationOptions {
   providers?: Array<
     | ContainerInitializationProvider
+    | ContainerInitializationStaticProvider
   >;
 }
 
@@ -19,6 +26,15 @@ export function initContainer({
       return typeof provider === "function";
     })
     .forEach((provider) => container.bind(provider).to(provider));
+  providers
+    ?.filter(
+      function (provider): provider is ContainerInitializationStaticProvider {
+        return "useValue" in provider;
+      },
+    )
+    .forEach((provider) =>
+      container.bind(provider.provide).toConstantValue(provider.useValue),
+    );
 
   return container;
 }
