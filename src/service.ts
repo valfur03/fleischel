368de9c6aea05c9b7d26530bc1acf9d89639a4c5
Valentin <valentin.furmanek@gmail.com>
2024-02-14 21:00:31 +0100

    feat: fetch and add torrent to transmission client

--- src/service.ts
+++ src/service.ts
@@ -5,12 +5,18 @@ import {
 import { TYPES } from "./inversify/types";
 import { Container } from "inversify";
 import { Logger } from "winston";
+import { ArchLinuxProvider } from "./ArchLinuxProvider/arch-linux.provider";
+import { TransmissionAdapter } from "./TransmissionAdapter/transmission.adapter";
 
 function watch(container: Container) {
   return async () => {
     const logger = container.get<Logger>(TYPES.Logger);
 
     logger.info("Starting service");
+    const archLinuxProvider = container.get(ArchLinuxProvider);
+    const transmissionAdapter = container.get(TransmissionAdapter);
+    const torrent = await archLinuxProvider.getLatestTorrent();
+    await transmissionAdapter.addTorrent(torrent);
   };
 }
 
