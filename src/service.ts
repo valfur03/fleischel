e0fa383b494178738f63cead13fefdc137424326
Valentin <valentin.furmanek@gmail.com>
2024-02-15 22:14:18 +0100

    refactor: get adapters and providers as their base interface

--- src/service.ts
+++ src/service.ts
@@ -7,16 +7,16 @@ import { Container } from "inversify";
 import { Logger } from "winston";
 import { ArchLinuxProvider } from "./ArchLinuxProvider/arch-linux.provider";
 import { TransmissionAdapter } from "./TransmissionAdapter/transmission.adapter";
+import { Provider } from "./common/interfaces/provider";
+import { Adapter } from "./common/interfaces/adapter";
 
 function watch(container: Container) {
   return async () => {
     const logger = container.get<Logger>(TYPES.Logger);
 
     logger.info("Starting service");
-    const archLinuxProvider = container.get(ArchLinuxProvider);
-    const transmissionAdapter = container.get(TransmissionAdapter);
-    const torrent = await archLinuxProvider.getLatestTorrent();
-    await transmissionAdapter.addTorrent(torrent);
+    const archLinuxProvider = container.get<Provider>(ArchLinuxProvider);
+    const transmissionAdapter = container.get<Adapter>(TransmissionAdapter);
   };
 }
 
