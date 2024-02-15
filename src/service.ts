c99efe5725cab5cf1403e4ad031a10417f37dd9b
Valentin <valentin.furmanek@gmail.com>
2024-02-15 22:14:54 +0100

    feat: add a cron job that fetches new torrents

--- src/service.ts
+++ src/service.ts
@@ -7,6 +7,7 @@ import { Container } from "inversify";
 import { Logger } from "winston";
 import { ArchLinuxProvider } from "./ArchLinuxProvider/arch-linux.provider";
 import { TransmissionAdapter } from "./TransmissionAdapter/transmission.adapter";
+import { CronJob } from "cron";
 import { Provider } from "./common/interfaces/provider";
 import { Adapter } from "./common/interfaces/adapter";
 
@@ -17,6 +18,18 @@ function watch(container: Container) {
     logger.info("Starting service");
     const archLinuxProvider = container.get<Provider>(ArchLinuxProvider);
     const transmissionAdapter = container.get<Adapter>(TransmissionAdapter);
+
+    const cron = await archLinuxProvider.getCron();
+    const job = CronJob.from({
+      cronTime: typeof cron === "string" ? cron : cron.cronTime,
+      runOnInit: true,
+      onTick: async () => {
+        const torrent = await archLinuxProvider.getLatestTorrent();
+        await transmissionAdapter.addTorrent(torrent);
+      },
+      timeZone: typeof cron !== "string" ? cron.timeZone : undefined,
+    });
+    job.start();
   };
 }
 
