62886cd7eb6b920825a461a1f5873ef3efa0a5a5
Valentin <valentin.furmanek@gmail.com>
2024-02-15 22:30:22 +0100

    refactor: move method to initialize all jobs in another file

--- src/service.ts
+++ src/service.ts
@@ -5,36 +5,24 @@ import {
 import { TYPES } from "./inversify/types";
 import { Container } from "inversify";
 import { Logger } from "winston";
-import { ArchLinuxProvider } from "./ArchLinuxProvider/arch-linux.provider";
-import { TransmissionAdapter } from "./TransmissionAdapter/transmission.adapter";
+import { initJobs } from "./jobs";
 import { CronJob } from "cron";
-import { Provider } from "./common/interfaces/provider";
-import { Adapter } from "./common/interfaces/adapter";
 
-function watch(container: Container) {
+function watch(container: Container, jobs: Array<CronJob>) {
   return async () => {
     const logger = container.get<Logger>(TYPES.Logger);
 
     logger.info("Starting service");
-    const archLinuxProvider = container.get<Provider>(ArchLinuxProvider);
-    const transmissionAdapter = container.get<Adapter>(TransmissionAdapter);
 
-    const cron = await archLinuxProvider.getCron();
-    const job = CronJob.from({
-      cronTime: typeof cron === "string" ? cron : cron.cronTime,
-      runOnInit: true,
-      onTick: async () => {
-        const torrent = await archLinuxProvider.getLatestTorrent();
-        await transmissionAdapter.addTorrent(torrent);
-      },
-      timeZone: typeof cron !== "string" ? cron.timeZone : undefined,
+    jobs.forEach((job) => {
+      job.start();
     });
-    job.start();
   };
 }
 
 export function createService(options: ContainerInitializationOptions) {
   const container = initContainer(options);
+  const jobs = initJobs(container);
 
-  return { watch: watch(container) };
+  return { watch: watch(container, jobs) };
 }
