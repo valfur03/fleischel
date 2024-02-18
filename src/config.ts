df46e4bf6d0b16d148d00e647c65912c7118f117
Valentin <valentin.furmanek@gmail.com>
2024-02-18 12:09:05 +0100

    FIXUP retry

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,10 @@ function parseIntWithDefault(string: string | undefined, defaultValue: number) {
 }
 
 export const config = {
+  unchangedTorrentRetryTime: parseIntWithDefault(
+    process.env.UNCHANGED_TORRENT_RETRY_TIME,
+    1000 * 60 * 60,
+  ),
   transmission: {
     host: process.env.TRANSMISSION_HOST,
     port: process.env.TRANSMISSION_PORT || 9091,
